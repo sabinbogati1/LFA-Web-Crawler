b8650e2ba92f11c172346a68de93afb14dd71179
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-02-24 10:07:02 +0545

    Complete Single Page Crawler

--- src/lfawebcrawler/LFAWebCrawler.java
+++ src/lfawebcrawler/LFAWebCrawler.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LFAWebCrawler {
 
@@ -20,13 +22,25 @@ public class LFAWebCrawler {
             StringBuilder content = new StringBuilder();
 
             String line = "";
-            
-            
-            while((line=reader.readLine())!=null){
-                System.out.println(line);
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");//jasta ko testai
             }
             reader.close();
 
+            String regEx = "<a href=\"(.*?)\" class=\"track\" (.*?)>\\n(.*?)<span class=\"right\"></span>\\n(.*?)<h5>(.*?)</h5>";
+
+            Pattern pattern = Pattern.compile(regEx);
+            
+            Matcher matcher = pattern.matcher(content.toString());
+            
+            while(matcher.find()){
+                
+                 System.out.println("TITLE: "+matcher.group(5));
+                System.out.println("LINK: "+matcher.group(1));
+               
+            }
+            
         } catch (IOException ioe) {
             System.out.println("Exception Catch: " + ioe.getMessage());
         }
